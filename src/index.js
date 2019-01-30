cc42b6bdae423227011179719ffd9d214e3bab2d
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-01-30 13:54:51 -0500

    Displayed names on the page and a placeholder loader

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
 import rootReducer from "./reducers";
 // needed dependancies
 // applyMiddleware from redux
@@ -11,9 +13,14 @@ import rootReducer from "./reducers";
 // logger from redux-logger
 // rootReducer from ./reducers
 
+const middleWare = [thunk, logger];
 const store = createStore(
-  rootReducer
+  rootReducer,
   /* applyMiddleware goes here */
+  compose(
+    applyMiddleware(...middleWare),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
 );
 
 ReactDOM.render(
--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -3,33 +3,37 @@ import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
 // import actions
+import { getData } from "../actions/index";
 
 class CharacterListView extends React.Component {
-  constructor() {
-    super();
-  }
-
   componentDidMount() {
-    // call our action
+    this.props.getData();
   }
 
   render() {
-    if (this.props.fetching) {
-      // return something here to indicate that you are fetching data
+    if (this.props.isLoading) {
+      return <div>LOADING</div>;
     }
     return (
       <div className="CharactersList_wrapper">
-        <CharacterList characters={this.props.characters} />
+        {<CharacterList characters={this.props.characters} />}
       </div>
     );
   }
 }
 
+const mapStateToProps = state => ({
+  characters: state.charsReducer.characters,
+  isLoading: state.charsReducer.isLoading
+});
+
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
 export default connect(
-  null /* mapStateToProps replaces null here */,
-  {
-    /* action creators go here */
-  }
+  mapStateToProps /* mapStateToProps replaces null here */,
+  { getData }
 )(CharacterListView);
+
+///(
+///<CharacterList characters={this.props.characters} />
+///)
